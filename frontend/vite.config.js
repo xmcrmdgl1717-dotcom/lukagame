f633cac2ac36dab44e5f55905563e71f4a7e15bd
xmcrmdgl1717-dotcom <xmcrmdgl1717@gmail.com>
2026-09-21 22:07:55 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
+module.exports = defineConfig({ plugins: [react()] });
