62f8a66b08c0ab313bbd5998ee30c059ad5debff
xmcrmdgl1717-dotcom <xmcrmdgl1717@gmail.com>
2026-09-24 23:34:23 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { captureUtm } from './utils/utm';
+import { installErrorReporter } from './utils/errorReporter';
 
-// 在应用启动前，立即捕获 URL 中的 UTM 参数并存入 localStorage
+// 捕获 UTM
 captureUtm();
+
+// 安装全局错误捕获
+installErrorReporter();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
